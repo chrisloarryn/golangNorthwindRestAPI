2fdd5929fe630ba03408481f8bba250c3ae847c4
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2020-06-06 13:44:53 -0400

    commit

--- database/database.go
+++ database/database.go
@@ -3,7 +3,7 @@ package database
 import "database/sql"
 
 func InitDB() *sql.DB {
-	connectionString := "root:qwerty@tcp(localhost:3306)/northwind"
+	connectionString := "root:qwerty@tcp(127.0.0.1:3306)/northwind"
 	databaseConnection, err := sql.Open("mysql", connectionString)
 
 	if err != nil {
--- main.go
+++ main.go
@@ -1,18 +1,68 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	// "fmt"
 	"golangNorthwindRestAPI/database"
-	"fmt"
+
+	"net/http"
+
+	"github.com/go-chi/chi"
+	// "github.com/go-chi/chi/middleware"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var databaseConnection *sql.DB
+
+type Product struct {
+	ID           int    `json:"id"`
+	Product_Code string `json:"product_code"`
+	Description  string `json:"description"`
+}
+
+func catch (err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	databaseConnection := database.InitDB()
+	defer databaseConnection.Close()
 
 	//Logica
 
-	defer databaseConnection.Close()
+	r := chi.NewRouter()
+	
+	r.Get("/products", AllProducts)
 
-	fmt.Println(databaseConnection)
+	http.ListenAndServe(":5000", r)
+
+	//fmt.Println(databaseConnection)
+}
+
+func AllProducts(w http.ResponseWriter, r *http.Request) {
+	const sql = `SELECT id,product_code,COALESCE(description,'')
+				 FROM products`
+	results, err := databaseConnection.Query(sql)
+	catch(err)
+	var products []*Product
+
+	for results.Next() {
+		product := &Product{}
+		err = results.Scan(&product.ID, &product.Product_Code, &product.Description)
+
+		catch(err)
+		products = append(products, product)
+	}
+	respondwithJSON(w, http.StatusOK, products)
+}
+
+func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
